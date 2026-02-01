900f1e54959d98d0bce12e5c482f79e56acf8ddd
Danielle Rivera <drivera2288@gmail.com>
2026-02-01 12:53:35 -0500

    Add files via upload

---
+++ SVDQuartets/run_paup.sh
@@ -0,0 +1,16 @@
+
+## Run paup.
+
+## Resources:
+## http://www.phylosolutions.com/tutorials/ssb2018/svdquartets-tutorial.html
+## https://evomics.org/learning/population-and-speciation-genomics/2018-population-and-speciation-genomics/svdquartets-astral-activity/
+## https://phylosolutions.com/tutorials/svdq-qage/svdq-qage-tutorial.html
+
+## Folder:
+cd ~/Analyses/svdq
+
+## Read file and run analysis:
+## Species assignments added by hand into file.
+/Users/drivera/Documents/paup4a168_osx rgran.nex -L paup.log
+
+## End of script.
